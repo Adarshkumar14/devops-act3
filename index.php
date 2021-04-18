0a12b3053743c072608b7eb3b437ca855cc22f17
Adarshkumar14 <56665829+Adarshkumar14@users.noreply.github.com>
2021-04-18 18:45:52 +0530

    commit3

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-echo 'User IP Address : '. $_SERVER['REMOTE_ADDR'];
+
+echo 'User IP Address : ' $_SERVER['REMOTE_ADDR'];
+
+$ip_server = $_SERVER['SERVER_ADDR'];
+echo "Server IP Address is: $ip_server";
+
 ?>
   
